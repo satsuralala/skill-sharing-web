c1012c5b77cc5db5cedb2dd930a15fe554fd19f1
badral <badral@badrals-MacBook-Pro.local>
2025-01-19 16:47:37 +0800

    t

--- resolvers/mutations/user/register.ts
+++ resolvers/mutations/user/register.ts
@@ -1,21 +1,31 @@
-
 import { userModel } from "../../../models/all/user.model";
 import bcrypt from "bcryptjs";
 import { GraphQLError } from "graphql";
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
-interface Input{
-    name: String,
-    email: String,
-    password: String,
+interface Input {
+  name: String;
+  email: String;
+  password: String;
 }
 
-export const register=async(_:any,{name,email,password}:Input)=>{
-   if(!name || !email || !password) throw new GraphQLError('Please make sure all input fields are entered.')
-    const PASS_SALT=process.env.PASS_SALT;
-    const hashedPassword=await bcrypt.hash(String(password), Number(PASS_SALT))
-    const user=await userModel.create({name,email,password:hashedPassword});
-    const TOKEN_SECRET = process.env.TOKEN_SECRET || '';
-    const token = await jwt.sign({ userId: user._id, email: user.email }, TOKEN_SECRET, { expiresIn: '1d' });
-    return {token}
-}
+export const register = async (_: any, { name, email, password }: Input) => {
+  if (!name || !email || !password)
+    throw new GraphQLError("Please make sure all input fields are entered.");
+
+  const PASS_SALT = process.env.PASS_SALT;
+  const hashedPassword = await bcrypt.hash(String(password), Number(PASS_SALT));
+  const user = await userModel.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+  const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
+  const token = await jwt.sign(
+    { userId: user._id, email: user.email },
+    TOKEN_SECRET,
+    { expiresIn: "1d" }
+  );
+  console.log(token);
+  return { token };
+};
--- src/app/(public)/register/page.tsx
+++ src/app/(public)/register/page.tsx
@@ -1,77 +1,134 @@
-'use client'
+"use client";
 
-import { Input } from '@/components/shadcn/input';
-import { gql, useMutation } from '@apollo/client';
-import Image from 'next/image';
-import Link from 'next/link';
-import { useState } from 'react';
-
+import { Input } from "@/components/shadcn/input";
+import { gql, useMutation } from "@apollo/client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
 
 const REGISTER = gql`
   mutation register($name: String!, $email: String!, $password: String!) {
-  register(name: $name, email: $email, password: $password) {
-    token
+    register(name: $name, email: $email, password: $password) {
+      token
+    }
   }
-}
 `;
 
-const Register=()=> {
-  const [email,setEmail]=useState('');
-  const [name,setName]=useState('');
-  const [password,setPassword]=useState('');
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
+  const [password, setPassword] = useState("");
 
-  const [register,{loading}] = useMutation(REGISTER, {
-    onCompleted:async(data)=>{
-      localStorage.setItem('token',data.token);
-      window.location.href = '/signIn'
+  const [register, { loading }] = useMutation(REGISTER, {
+    onCompleted: async (data) => {
+      console.log({ data });
+      localStorage.setItem("token", data.register.token);
+      window.location.href = "/signIn";
     },
     variables: {
-      name:name,
-      email:email,
-      password:password,
+      name: name,
+      email: email,
+      password: password,
     },
   });
 
   return (
-    <div data-cy="register-page-container" className="pt-[150px] justify-items-center">
-    <div data-cy="register-email-header" className="flex items-center gap-1">
-      <Image src="/logo.svg" width={20} height={24} alt="logo" className="w-5 h-6" />
-      <div className="text-[#424242] font-bold text-2xl">Skill sharing</div>
-    </div>
-    <div className="text-[#09090B] font-semibold text-2xl pt-6 ">Create an account</div>
-    <div className="text-[#71717A] text-sm font-normal pt-1 ">Enter your email, name and password below to create your account</div>
-    <div className="pt-6">
-      <div className='flex flex-col gap-1'>
-      <div className="text-[#09090B] font-medium text-sm pb-1">Name</div>
-      <Input data-cy="register-email-input" placeholder="Naraa" className="w-[350px] border-[#E4E4E7] border-2 text-black bg-white" value={name} onChange={(e) => setName(e.target.value)} />
-      <div className="text-[#09090B] font-medium text-sm pb-1">Email</div>
-      <Input data-cy="register-email-input" placeholder="naraa@example.com" className="w-[350px] border-[#E4E4E7] text-black border-2 bg-white" value={email} onChange={(e) => setEmail(e.target.value)} />
-      <div className="text-[#09090B] font-medium text-sm pb-1">Password</div>
-      <Input data-cy="register-email-input" placeholder="Naraa1213" className="w-[350px] border-[#E4E4E7] border-2 text-black bg-white" value={password} onChange={(e) => setPassword(e.target.value)} />
-
+    <div
+      data-cy="register-page-container"
+      className="pt-[150px] justify-items-center"
+    >
+      <div data-cy="register-email-header" className="flex items-center gap-1">
+        <Image
+          src="/logo.svg"
+          width={20}
+          height={24}
+          alt="logo"
+          className="w-5 h-6"
+        />
+        <div className="text-[#424242] font-bold text-2xl">Skill sharing</div>
       </div>
-      <button data-cy="register-continue-button" className="w-[350px] h-9 bg-black  hover:bg-slate-700 rounded-full text-[#FAFAFA] text-sm font-medium mt-4" onClick={() => register()} disabled={loading}>
-        {loading ? <Image src="/spinner.svg" alt="loading" width={20} height={20} className="animate-spin" /> : 'Continue'}
-      </button>
-      <div className="flex">
-      
+      <div className="text-[#09090B] font-semibold text-2xl pt-6 ">
+        Create an account
       </div>
-      <Link href="/signIn">
-        <button data-cy="signIn" className="w-[350px] h-9 bg-white  border-[#E4E4E7] rounded-full text-[#18181B] hover:bg-white border-2 text-sm font-medium mt-4">
-          Log in
+      <div className="text-[#71717A] text-sm font-normal pt-1 ">
+        Enter your email, name and password below to create your account
+      </div>
+      <div className="pt-6">
+        <div className="flex flex-col gap-1">
+          <div className="text-[#09090B] font-medium text-sm pb-1">Name</div>
+          <Input
+            data-cy="register-email-input"
+            placeholder="Naraa"
+            className="w-[350px] border-[#E4E4E7] border-2 text-black bg-white"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <div className="text-[#09090B] font-medium text-sm pb-1">Email</div>
+          <Input
+            data-cy="register-email-input"
+            placeholder="naraa@example.com"
+            className="w-[350px] border-[#E4E4E7] text-black border-2 bg-white"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <div className="text-[#09090B] font-medium text-sm pb-1">
+            Password
+          </div>
+          <Input
+            data-cy="register-email-input"
+            placeholder="Naraa1213"
+            className="w-[350px] border-[#E4E4E7] border-2 text-black bg-white"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button
+          data-cy="register-continue-button"
+          className="w-[350px] h-9 bg-black  hover:bg-slate-700 rounded-full text-[#FAFAFA] text-sm font-medium mt-4"
+          onClick={() => register()}
+          disabled={loading}
+        >
+          {loading ? (
+            <Image
+              src="/spinner.svg"
+              alt="loading"
+              width={20}
+              height={20}
+              className="animate-spin"
+            />
+          ) : (
+            "Continue"
+          )}
         </button>
-      </Link>
+        <div className="flex"></div>
+        <Link href="/signIn">
+          <button
+            data-cy="signIn"
+            className="w-[350px] h-9 bg-white  border-[#E4E4E7] rounded-full text-[#18181B] hover:bg-white border-2 text-sm font-medium mt-4"
+          >
+            Log in
+          </button>
+        </Link>
 
-      <div className="text-[#71717A] font-normal text-sm pt-6 text-center">By clicking continue, you agree to our</div>
-      <div className="flex justify-center">
-        <div className="text-[#71717A] font-normal text-sm text-center underline-offset-1 underline">Terms of Service </div>
-        <div className="text-[#71717A] font-normal text-sm text-center mx-1"> and</div>
-        <div className="text-[#71717A] font-normal text-sm text-center underline-offset-1 underline"> Privacy Policy.</div>
+        <div className="text-[#71717A] font-normal text-sm pt-6 text-center">
+          By clicking continue, you agree to our
+        </div>
+        <div className="flex justify-center">
+          <div className="text-[#71717A] font-normal text-sm text-center underline-offset-1 underline">
+            Terms of Service{" "}
+          </div>
+          <div className="text-[#71717A] font-normal text-sm text-center mx-1">
+            {" "}
+            and
+          </div>
+          <div className="text-[#71717A] font-normal text-sm text-center underline-offset-1 underline">
+            {" "}
+            Privacy Policy.
+          </div>
+        </div>
       </div>
     </div>
-    
-  </div>
   );
-}
+};
 
 export default Register;
