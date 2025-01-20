a6204e871a44b728b35fe309d7f02c698ae7d4f1
satsuralala <satsuraltumurbat@gmail.com>
2025-01-20 17:23:03 +0800

    fixed error again in regisgter

--- resolvers/mutations/user/register.ts
+++ resolvers/mutations/user/register.ts
@@ -9,7 +9,6 @@ interface Input {
   password: String;
 }
 
-<<<<<<< HEAD
 export const register=async(_:any,{name,email,password}:Input)=>{
    if(!name || !email || !password) throw new GraphQLError('Please make sure all input fields are entered.')
     const PASS_SALT=process.env.PASS_SALT;
@@ -19,25 +18,3 @@ export const register=async(_:any,{name,email,password}:Input)=>{
     const token = await jwt.sign({ userId: user._id, email: user.email, }, TOKEN_SECRET, { expiresIn: '1d' });
     return {token}
 }
-=======
-export const register = async (_: any, { name, email, password }: Input) => {
-  if (!name || !email || !password)
-    throw new GraphQLError("Please make sure all input fields are entered.");
-
-  const PASS_SALT = process.env.PASS_SALT;
-  const hashedPassword = await bcrypt.hash(String(password), Number(PASS_SALT));
-  const user = await userModel.create({
-    name,
-    email,
-    password: hashedPassword,
-  });
-  const TOKEN_SECRET = process.env.TOKEN_SECRET || "";
-  const token = await jwt.sign(
-    { userId: user._id, email: user.email },
-    TOKEN_SECRET,
-    { expiresIn: "1d" }
-  );
-  console.log(token);
-  return { token };
-};
->>>>>>> f4ce05c0de11b02f9f0a5808ace4cb0c352b84ba
