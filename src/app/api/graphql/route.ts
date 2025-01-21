0d1f069e296a2f1c882b33bfc7525d0a0a560e76
satsuralala <satsuraltumurbat@gmail.com>
2025-01-21 11:28:14 +0800

    changed handler and route

--- src/app/api/graphql/route.ts
+++ src/app/api/graphql/route.ts
@@ -1,16 +1,12 @@
-// src/app/api/graphql/route.ts
-
 export const dynamic = 'force-dynamic';
 
 import { handler } from '../../../../src/handler';
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  // Ensure the handler works for GET requests.
-  return handler(request); // Pass the request to your handler.
+  return handler(request);
 }
 
 export async function POST(request: NextRequest) {
-  // Similarly, handle POST requests
   return handler(request);
 }
